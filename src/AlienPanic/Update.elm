e5d7d8b42a9ba5548bdd7199ce1c9c2a82d289bb
Hadi Moshayedi <hadi@moshayedi.net>
2015-11-30 22:01:22 -0500

    Reduce max hole depth to 0.6

--- src/AlienPanic/Update.elm
+++ src/AlienPanic/Update.elm
@@ -42,7 +42,7 @@ hole_coord player =
 dig_hole holes pos =
   holes |>
     map (\hole -> if (pos_eq hole.pos pos) then
-                   {hole|depth=min (hole.depth+0.2) 0.8}
+                   {hole|depth=min (hole.depth+0.2) 0.6}
                  else
                    hole)
 
