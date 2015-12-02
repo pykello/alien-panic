2a894d847f01c05a3afc4bf9cf1e3c63b72fdee3
Hadi Moshayedi <hadi@moshayedi.net>
2015-12-01 23:36:44 -0500

    Remove some unused functions.

--- src/AlienPanic/Rect.elm
+++ src/AlienPanic/Rect.elm
@@ -25,11 +25,6 @@ left_x (x, _, w, _) =
 right_x (x, _, w, _) =
   x + w * 0.5
 
-is_under: Rect -> Rect -> Bool
-is_under rect1 rect2 =
-  range_overlaps (x_range rect2) (x_range rect1) &&
-  range_overlaps (y_range_top rect2) (bottom rect1, bottom rect1)
-
 overlaps rect1 rect2 =
   range_overlaps (x_range rect1) (x_range rect2) &&
   range_overlaps (y_range rect1) (y_range rect2)
@@ -44,15 +39,6 @@ x_range (x, _, w, _) =
 
 y_range (_, y, _, h) =
   (y - h * 0.5, y + h * 0.5)
-
-y_range_top (_, y, _, h) =
-  (y, y + h * 0.5)
-
-top_half (x, y, w, h) =
-  (x, y+h*0.25, w, h*0.5)
-
-bottom rect =
-  fst (y_range rect)
 
 range_overlaps (a, b) (c, d) =
   (max a c) < (min b d) + eps
